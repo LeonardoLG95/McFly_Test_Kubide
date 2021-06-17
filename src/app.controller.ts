487bb04cd5ae306996b27aacc3c3cb74b7e1c2e8
Leo <leonardo-lg@hotmail.com>
2021-06-17 13:49:00 +0200

    feat: endpoints to tag
    added endpoints to swagger tag

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,4 +1,5 @@
 import { Controller, Delete, Get, Post, Put } from '@nestjs/common';
+import { ApiTags } from '@nestjs/swagger';
 import { AppService } from './app.service';
 
 @Controller()
@@ -6,26 +7,31 @@ export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Post('/publish')
+  @ApiTags('notes')
   new(): string {
     return this.appService.new();
   }
 
   @Get('/all')
+  @ApiTags('notes')
   all(): string {
     return this.appService.all();
   }
 
   @Get('/note/:id')
+  @ApiTags('notes')
   one(): string {
     return this.appService.one();
   }
 
   @Put('/favourite/:id')
+  @ApiTags('notes')
   favourite(): string {
     return this.appService.favourite();
   }
 
   @Get('/favourites')
+  @ApiTags('notes')
   favourites(): string {
     return this.appService.favourites();
   }
