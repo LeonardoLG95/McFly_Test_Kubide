a278fc7f712e5692644c90d434dfc37c3ab8fd22
Leo <leonardo-lg@hotmail.com>
2021-06-17 16:47:13 +0200

    feat: first working endpoint
    add note works properly, failed orm use

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,38 +1,7 @@
-import { Controller, Delete, Get, Post, Put } from '@nestjs/common';
-import { ApiTags } from '@nestjs/swagger';
+import { Controller} from '@nestjs/common';
 import { AppService } from './app.service';
 
-@Controller()
+@Controller('notes')
 export class AppController {
   constructor(private readonly appService: AppService) {}
-
-  @Post('/publish')
-  @ApiTags('notes')
-  new(): string {
-    return this.appService.new();
-  }
-
-  @Get('/all')
-  @ApiTags('notes')
-  all(): string {
-    return this.appService.all();
-  }
-
-  @Get('/note/:id')
-  @ApiTags('notes')
-  one(): string {
-    return this.appService.one();
-  }
-
-  @Put('/favourite/:id')
-  @ApiTags('notes')
-  favourite(): string {
-    return this.appService.favourite();
-  }
-
-  @Get('/favourites')
-  @ApiTags('notes')
-  favourites(): string {
-    return this.appService.favourites();
-  }
 }
