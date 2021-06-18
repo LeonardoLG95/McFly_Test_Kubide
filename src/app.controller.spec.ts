1e95b70836a55aca38e6358b7a2f2474a63211d5
Leo <leonardo-lg@hotmail.com>
2021-06-18 13:04:42 +0200

    feat: added better description and some fixes
    fixed the type of returns a string not an instance of a specific type

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -2,7 +2,7 @@ import { Test } from '@nestjs/testing';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
-describe('Testing AppController and AppService', () => {
+describe('Testing AppController and AppService: ', () => {
   let appController: AppController;
 
   beforeEach(async () => {
@@ -14,34 +14,28 @@ describe('Testing AppController and AppService', () => {
     appController = moduleRef.get<AppController>(AppController);
   });
 
-  describe('Testing controller endpoints and their functions in service: ', () => {
-    it('Testing add/:note should return nothing, is an insert.', () => {
-      expect(typeof appController.addNote('Testing note')).toBe(null);
+  describe('Endpoints with their functions:  ', () => {
+    it('add/:note should return nothing, is an insert.', () => {
+      expect(typeof appController.addNote('Testing note')).toBe("undefined");
     });
 
-    it('Testing /all endpoint should return an array of JSON with all the messages.', () => {
+    it('/all endpoint should return an array of JSON with all the messages.', () => {
       const result = appController.all()
-      expect(typeof result).toBe(Object);
-
-      if(result) console.log(result);
-      else console.log('No finding register, maybe fail testing adding note.');
+      expect(typeof result).toBe("object");
     });
 
-    it('Testing /:id should return an array with one JSON.', () => {
+    it('/:id should return an array with one JSON.', () => {
       const result = appController.one(1);
-      expect(typeof result).toBe(Object);
-
-      if(result) console.log(result);
-      else console.log('No finding register, maybe fail testing adding note.');
+      expect(typeof result).toBe("object");
     });
 
-    it('Testing /markFavourite/:id should return nothing, is an update.', () => {
-      expect(typeof appController.markFavourite(1)).toBe(null);
+    it('/markFavourite/:id should return nothing, is an update.', () => {
+      expect(typeof appController.markFavourite(1)).toBe("undefined");
     });
 
-    it('Testing /favourites should return an array of JSON with \
+    it('/favourites should return an array of JSON with \
     all the messages marked as favourites.', () => {
-      expect(typeof appController.markFavourite(1)).toBe(Object);
+      expect(typeof appController.favourites()).toBe("object");
     });
   });
 });
