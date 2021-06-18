56d68f4dad12549bc599e6f086578e8ab915c822
Leo <leonardo-lg@hotmail.com>
2021-06-18 12:44:35 +0200

    feat: added unit testing
    added unit testing for the controller and the service

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -1,22 +1,47 @@
-import { Test, TestingModule } from '@nestjs/testing';
+import { Test } from '@nestjs/testing';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
-describe('AppController', () => {
+describe('Testing AppController and AppService', () => {
   let appController: AppController;
 
   beforeEach(async () => {
-    const app: TestingModule = await Test.createTestingModule({
+    const moduleRef = await Test.createTestingModule({
       controllers: [AppController],
       providers: [AppService],
     }).compile();
 
-    appController = app.get<AppController>(AppController);
+    appController = moduleRef.get<AppController>(AppController);
   });
 
-  describe('root', () => {
-    it('should return "Hello World!"', () => {
-      expect(appController.getHello()).toBe('Hello World!');
+  describe('Testing controller endpoints and their functions in service: ', () => {
+    it('Testing add/:note should return nothing, is an insert.', () => {
+      expect(typeof appController.addNote('Testing note')).toBe(null);
+    });
+
+    it('Testing /all endpoint should return an array of JSON with all the messages.', () => {
+      const result = appController.all()
+      expect(typeof result).toBe(Object);
+
+      if(result) console.log(result);
+      else console.log('No finding register, maybe fail testing adding note.');
+    });
+
+    it('Testing /:id should return an array with one JSON.', () => {
+      const result = appController.one(1);
+      expect(typeof result).toBe(Object);
+
+      if(result) console.log(result);
+      else console.log('No finding register, maybe fail testing adding note.');
+    });
+
+    it('Testing /markFavourite/:id should return nothing, is an update.', () => {
+      expect(typeof appController.markFavourite(1)).toBe(null);
+    });
+
+    it('Testing /favourites should return an array of JSON with \
+    all the messages marked as favourites.', () => {
+      expect(typeof appController.markFavourite(1)).toBe(Object);
     });
   });
 });
